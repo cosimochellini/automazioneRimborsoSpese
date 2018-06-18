bc1bc342ca83d7a19c981df898524cb26c158852
INLDOM\c.chellini <c.chellini@inlinea.net>
2018-06-18 16:40:07 +0200

    modifiche varie

---
+++ controller/pdf.js
@@ -0,0 +1,9 @@
+var office = require('office-script');
+
+const pdf = {};
+
+pdf.createPdf = async (settings) => {
+    var of = office;
+};
+
+module.exports = pdf;
--- controller/rimborsoSpese.js
+++ controller/rimborsoSpese.js
@@ -1,19 +1,30 @@
-const timecard = require("../controller/timecard");
+const pdf = require("../controller/pdf");
 const excel = require("../controller/excel");
+const timecard = require("../controller/timecard");
+
 const rimborsoSpese = {};
 
 rimborsoSpese.execute = async (settings) => {
     const token = await timecard.getToken(settings);
+    process.stdout.write("token acquired \n");
 
     const entries = await timecard.getTimecards(settings, token);
+    process.stdout.write("timecard acquired \n");
 
     const filteredEntries = entries.filter((item) => timecard.filterFunction(item, settings));
+    process.stdout.write("timecard filtered \n");
 
     const worbook = await excel.importWorkbook(settings);
+    process.stdout.write("workbook imported\n");
 
     const compiledWorkbook = await excel.compileWorkbook(settings, worbook, filteredEntries);
+    process.stdout.write("workbook compiled\n");
 
     const fileNamePath = await excel.exportWorkbook(settings, compiledWorkbook);
+    process.stdout.write("workbook exported\n");
+
+    const result = pdf.createPdf(settings);
+
 };
 
 
