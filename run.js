78edf57a68ab8871ddf9fd7e80b2ddfbc9015dec
INLDOM\c.chellini <c.chellini@inlinea.net>
2018-06-27 17:45:29 +0200

    modifica

--- run.js
+++ run.js
@@ -4,8 +4,6 @@ const rimborsoSpese = require("./controller/rimborsoSpese");
 
 const settings = JSON.parse(fs.readFileSync('./settings/settings.json'));
 
-rimborsoSpese.execute(settings, __dirname).then(result => {
-
-    result ? process.stdout.write("script runned correctly\n") : process.stdout.write("script failed\n")
-
-}).catch(error => process.stdout.write(error));
+rimborsoSpese.execute(settings, __dirname)
+    .then(() => process.stdout.write("script runned correctly\n"))
+    .catch(error => process.stdout.write(error));
